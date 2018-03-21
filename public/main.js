2d44372a68b3b0b53643733d2ddef3f68ca0a0f4
Rigney <krigney@proforma.com>
2018-03-21 15:45:48 -0400

    I refactored my code and it works!

--- public/main.js
+++ public/main.js
@@ -14,43 +14,35 @@ const weatherSearch = () => {
   }
 }
 
-//if it is a ZIP code, fetch the data for the ZIP code 
-
+//if zip, get API URL for zip
 
 const pullWeatherForZIP = (zipCode) => {
   const weatherURLforZIP = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&APPID=77cb4f5c8963d2633252da804b410344&units=imperial";
-
   console.log("starting request");
-  fetch(weatherURLforZIP).then((response) => {
-    console.log("response came back", response)
-    if (response.status === 200) {
-      return response.json();
-    }
-  }).then((weatherForZIP) => {
-    console.log(weatherForZIP);
-//add element to the DOM
-    const output = document.querySelector(".weatherDataDisplay")
-    output.textContent = `It is currently ${weatherForZIP.main.temp} degrees in ${weatherForZIP.name}`;
-  })
-  console.log('fired request');
+  pullWeather(weatherURLforZIP);
 }
 
-//if search is a city, fetch the data for the city.
-
+//if city, get API URL for city
 
 const pullWeatherForCity = (cityName) => {
   const weatherURLforCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=77cb4f5c8963d2633252da804b410344&units=imperial";
-
   console.log("starting request");
-  fetch(weatherURLforCity).then((response) => {
+  pullWeather(weatherURLforCity);
+}
+
+//using APIs, pull and display weather date for search
+
+const pullWeather = (URL) => {
+  fetch(URL).then((response) => {
     console.log("response came back", response)
     if (response.status === 200) {
       return response.json();
     }
-  }).then((weatherForCity) => {
-    console.log(weatherForCity);
+  }).then((weather) => {
+    console.log(weather);
+//add element to the DOM
     const output = document.querySelector(".weatherDataDisplay")
-    output.textContent = `It is currently ${weatherForCity.main.temp} degrees with ${weatherForCity.main.humidity}% humidity in ${weatherForCity.name}`;
+    output.textContent = `It is currently ${weather.main.temp} degrees and ${weather.weather[0].main} in ${weather.name}`;
   })
   console.log('fired request');
 }
